17febcf97de542c797f54c3f33661da7444e9911
Orlina M Brabar <90068655+OrLina1125@users.noreply.github.com>
2022-04-30 09:31:40 +0900

    add various condition statements

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+</head>
+<body>
+
+<?php
+$x = 1;
+if($x == 1){
+echo "Daniel is very handsome!";
+}
+elseif($x == 2){
+    echo "Daniel is kind handsome!";
+}
+elseif($x == 3){
+    echo "Daniel is kind handsome!";
+}
+elseif($x == 4){
+    echo "Daniel is kind handsome!";
+}
+else {
+    echo "Daniel is very ugly!";
+
+}
+
+?>
+
+</body>
+</html>
+
